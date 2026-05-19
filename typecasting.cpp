bcc1bbed205ab1790b1fb5ef1cdf9d0d0cb6173c
Aruti <arutitiwari@gmail.com>
2026-05-20 00:30:20 +0530

    typecasting

---
+++ typecasting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char grade='a';  //97 vahi capital A ki alg value hogi
+
+    int value = grade;
+    cout <<value<<endl;
+    double price=100.99;
+    int newPrice= (int)price;
+    cout<< newPrice<<endl;
+    return 0;
+}
