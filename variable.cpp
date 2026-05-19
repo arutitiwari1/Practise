8be8166cb46e242b1e5aa57ba421d2451ceb0ee6
Aruti <arutitiwari@gmail.com>
2026-05-19 19:51:26 +0530

    primitive data types

--- variable.cpp
+++ variable.cpp
@@ -7,5 +7,11 @@ int main() {
      char grade= 'a';
      cout<< grade<<endl;
      cout<<sizeof(age)<<endl;
+     float PI=3.14f;
+     cout<<PI<<endl;
+     bool isSafe = true;
+     cout<<isSafe<<endl;   //true=1 false=0
+     double price=100.99;
+     cout<<price<<endl;
      return 0;
-} // age aur uske bytes print grades
+} // age aur uske bytes print grades float (decimal no )
