929a52936b58a00a110fe789b247f5166c01143e
Aruti <arutitiwari@gmail.com>
2026-05-19 15:04:31 +0530

    my first program

---
+++ code.cpp
@@ -0,0 +1,9 @@
+#include<iostream>  //imp to start
+using namespace std;
+
+int main(){
+    cout << "arutitiwari" <<"\n"; // iska matlab line alg hojati h 
+    cout << "hello"<<endl; // same as \n
+    cout<< "kushagra";
+    return 0;
+}
