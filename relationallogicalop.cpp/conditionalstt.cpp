77863f0d3bb26ea1f570d98a3652fb26ce7856d7
Aruti <arutitiwari@gmail.com>
2026-05-22 04:29:00 +0530

    if else conditional

---
+++ relationallogicalop.cpp/conditionalstt.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;  // n is neg or postive
+int main (){
+int n = 66;
+
+if ( n >=0) {cout<<"positive\n";
+}else{cout<<"negative\n";    // print positive hua h
+}
+return 0;
+}
