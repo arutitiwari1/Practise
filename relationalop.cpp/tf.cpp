75d65369f554befc1d56a2429b8d73f355491a74
Aruti <arutitiwari@gmail.com>
2026-05-22 02:43:52 +0530

    relational operators

---
+++ relationalop.cpp/tf.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<(3<9)<<endl; //1
+    cout<<(4>6)<<endl; //0
+    cout<<(3>=3)<<endl; //1     (dusri directory ke liye cd aur uska nam)
+    cout<<(3!=3)<<endl; //0 ans palat deta h not
+    return 0;
+}
