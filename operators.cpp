a0d84b3e6ed246f1e2cb2beab7c537636dab47f4
Aruti <arutitiwari@gmail.com>
2026-05-21 16:23:04 +0530

    operators.cpp

---
+++ operators.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=10,b=5;
+    
+    cout<<"sum="<<(a+b) <<endl;
+    cout<<"difference="<<(a-b)<<endl;
+    cout<<"Product="<<(a*b)<<endl;
+    cout<<"division="<<(a/b)<<endl; //2
+    cout<<"remainder="<<(a%b)<<endl; //0
+    return 0;
+} // 5/2=2.5 par ans 2
+ // 10/3=3.33 par ans 3
+ // bat int/float ho toh ans float aur double toh double 
+ 
