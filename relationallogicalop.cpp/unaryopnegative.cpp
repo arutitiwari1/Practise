28e150ca3c763f0d00389a209e53083628dfc423
Aruti <arutitiwari@gmail.com>
2026-05-22 04:12:17 +0530

    neg unary op

---
+++ relationallogicalop.cpp/unaryopnegative.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    int b=a--;
+    cout<<"a:"<<a<< endl; // a=9
+    cout<< "b:"<<b<< endl; // b=10     --a =b hota toh ans 9 hi hota dono me
+    return 0;
+
+}
