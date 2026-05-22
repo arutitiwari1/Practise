4f50bb16d705ffc3585cc11994120e731e6a11c4
Aruti <arutitiwari@gmail.com>
2026-05-22 17:18:17 +0530

    print no by wl

---
+++ loops/printnobywl.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int count=1;
+
+    while(count<=10){
+        cout<<count<<" ";
+        count++;
+
+    } cout<<endl;
+    return 0;    //no print 1 2 3 4 5 6 7 8 9 10
+}
