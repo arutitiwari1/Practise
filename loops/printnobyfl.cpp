64aa6f3d79c05392c5c9bc99db81b5f28fc8447e
Aruti <arutitiwari@gmail.com>
2026-05-22 17:28:52 +0530

    printnobyfl

---
+++ loops/printnobyfl.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< "enter no:"<<endl;
+    cin>>  n;    // isse print hojayege no!!!
+    {for(int i=1;i<=n;i++)
+    cout<<i<<" ";
+}
+    cout<< endl;
+    return 0;
+}
