3a9207953ae6853e8d54a30897cc013e05bea4b7
Aruti <arutitiwari@gmail.com>
2026-05-26 17:32:54 +0530

    nested loop

---
+++ patterns/printin5line.cpp
@@ -0,0 +1,10 @@
+#include<iostream>       //print 1234 in 4
+using namespace std;
+int main (){
+for(int i=1;i<=5;i++)
+{
+    cout<<"*****"<<endl;    
+   //***** ye 5 line me prit hojayega!!!!
+} return 0;
+
+}
