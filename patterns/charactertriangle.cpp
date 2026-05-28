a6152cb9d173e704686576cbee0e5e28baeff650
Aruti <arutitiwari@gmail.com>
2026-05-28 14:20:00 +0530

    character triangle1

---
+++ patterns/charactertriangle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>     //print A;BC;DEF;
+using namespace std;
+int main (){
+    int n;
+    cout<<"enter no:";
+    cin>>n;
+    char ch='A';
+    for(int i=0;i<n;i++)
+    {
+        for (int j=0;j<i+1;j++)
+        {
+            cout<< ch;
+            ch++;
+        } 
+        cout<<endl;
+    } 
+    return 0;
+}
