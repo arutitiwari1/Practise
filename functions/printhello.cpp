41f159d55e4e64600c38f73d2a5022989041b721
Aruti <arutitiwari@gmail.com>
2026-06-04 21:25:11 +0530

    function print hello

---
+++ functions/printhello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+//function definition
+void printHello(){      //void use toh return nahin use hora see
+    cout<<"hello\n";
+    
+}
+int main (){
+    //functioncall /invoke
+    printHello();
+    printHello();  //print=hello;hello
+    return 0;
+}
