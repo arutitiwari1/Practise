314de11492748b8742d57e14b2f5b1151c9f8ee0
Aruti <arutitiwari@gmail.com>
2026-06-04 22:55:33 +0530

    sumof2

---
+++ functions/sumof2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int sumof2(int a,int b){
+    int sum=a+b;
+    return sum;         //double hoto int ki jgh doublr/float
+
+}
+int main(){
+    cout<<sumof2(5,7)<<endl;   print=12;17
+    cout<<sumof2(9,8)<<endl;
+    return 0;
+}
