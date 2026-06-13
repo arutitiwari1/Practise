e283b62fd25a9bf375ec8f406a99eca393f4f075
Aruti <arutitiwari@gmail.com>
2026-06-13 13:04:03 +0530

    all data modifiers i one code

---
+++ bitwiseop.cpp/alldatamodifier.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int  main(){
+cout<<sizeof(int)<<endl;//4
+cout<<sizeof(long int)<<endl;//4
+cout<<sizeof(short int)<<endl;//2
+cout<<sizeof(long long int)<<endl;//8
+return 0;
+}
