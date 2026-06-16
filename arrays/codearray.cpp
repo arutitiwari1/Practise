c04b61495d9ee7d86edffc68cbddd69c703d3298
Aruti <arutitiwari@gmail.com>
2026-06-16 14:07:04 +0530

    codearray1

---
+++ arrays/codearray.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int marks[5] {12,14,20,1,88};
+    cout<<marks[0]<<endl;
+    cout<<marks[1]<<endl;
+    cout<<marks[2]<<endl;
+    cout<<marks[3]<<endl;
+    cout<<marks[4]<<endl;
+    return 0;    //12;14;20;1;88
+}
