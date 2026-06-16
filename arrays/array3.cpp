9718c8ac72938d6f2bf787e3b1042d75497b1184
Aruti <arutitiwari@gmail.com>
2026-06-16 17:38:11 +0530

    array 3 on size

---
+++ arrays/array3.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks[5]{12,33,42,2,44};
+    int size=5;//20
+    
+    cout<<sizeof(marks)<<endl;//loops run only 0 to size-1
+    return 0;
+}
