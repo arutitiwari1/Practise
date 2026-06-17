0c1fd849ac34f5803af9c4a2d3c14e44c70419e9
Aruti <arutitiwari@gmail.com>
2026-06-17 12:38:42 +0530

    largest and smallest

---
+++ arrays/array5.cpp
@@ -0,0 +1,17 @@
+#include<climits>
+#include<iostream>
+using namespace std;
+int main(){
+    int nums[]={22,44,-24,3,-5,8};
+    int size=6;
+    int smallest= INT_MAX;
+    int largest= INT_MIN;
+    for(int i=0;i<size;i++)
+    {//min max
+    smallest=min(nums[i],smallest);
+    largest=max(nums[i],largest);
+    }
+    cout<<"smallest:"<<smallest<<endl;
+    cout<<"largest:"<<largest<<endl;
+    return 0;
+}
