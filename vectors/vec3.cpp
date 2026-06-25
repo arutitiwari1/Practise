973567d47ea8c56124ff1246ae3f49361afa7b0f
Aruti <arutitiwari@gmail.com>
2026-06-25 18:35:44 +0530

    vector type 2

---
+++ vectors/vec3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>vec(5,0);
+    cout<<vec[0]<<endl;//0
+    cout<<vec[1]<<endl;//0
+    cout<<vec[2]<<endl;//0
+    return 0;
+}
