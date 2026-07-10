7c4aeda0bc671b6011c8acd488966f2d2cd6d4af
Aruti <arutitiwari@gmail.com>
2026-07-10 17:53:28 +0530

    brute force approach to print majority element

---
+++ pairsum and majority elemnt/bfamjele.cpp
@@ -0,0 +1,23 @@
+#include<iostream>//brute force approach of maj elemrnt 
+#include<vector>
+#include<climits>
+using namespace std;
+int majorityElement(vector<int>&nums){
+    int n=nums.size();
+    for(int val:nums){
+        int freq=0;
+        for(int ele:nums){
+            if(ele==val){
+                freq++;
+            }
+        }
+        if(freq>n/2){
+            return val;
+        }
+    }return -1;
+}
+int main(){
+    vector<int>nums={1,2,2,2,1,1,1,1,1,1,1};
+    cout<<majorityElement(nums);//1
+    return 0;
+}
