6f02dfb9ee8e5e5dfbe6382943d26175f3d20dad
Aruti <arutitiwari@gmail.com>
2026-07-18 21:05:53 +0530

    container with most water

---
+++ vectors/contmostwater.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+int maxArea(vector<int>&height){
+    int maxWater=0; //ans
+    for(int i=0;i<height.size();i++){
+        for(int j=i+1;j<height.size();j++){
+            int w=j-i;
+            int ht=min(height[i],height[j]);
+               {
+                int currWater=w*ht;//area
+                maxWater=max(maxWater,currWater);
+               }
+        }
+    }
+    return maxWater;
+}
+int main(){
+    vector<int>height={1,7,8,6,5,4};
+    cout<<maxArea(height)<<endl;
+    return 0;
+}
