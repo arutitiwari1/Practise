e66186e698c9950428cae4c80d11b079e7b97cd0
Aruti <arutitiwari@gmail.com>
2026-07-19 14:24:33 +0530

    container max water most optimal solution

---
+++ vectors/containeroptimalsol.cpp
@@ -0,0 +1,22 @@
+#include<iostream>//container max water optimal solution tc=O(n) 
+#include<vector>
+#include<climits>
+using namespace std;
+int maxArea(vector<int>height){
+    int maxWater=0;//ans
+    int lp=0,rp=height.size()-1;
+    while (lp<rp)
+    {
+        int w=rp-lp;
+        int ht=min(height[lp],height[rp]);
+        int currWater=w*ht;
+        maxWater=max(maxWater,currWater);
+        height[lp]<height[rp]? lp++:rp--;
+    }
+    return maxWater;
+}
+int main(){
+    vector<int>height={1,8,6,2,5,4,8,3,7};
+    cout<< maxArea(height)<<endl;//49
+    return 0;
+}
