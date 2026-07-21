8f8f125a47c053a71f35c7ae6a52e996c5b4a091
Aruti <arutitiwari@gmail.com>
2026-07-21 21:12:16 +0530

    optimal approach with time complexity constant

---
+++ productofarrayexcptitself/optimalapproach.cpp
@@ -0,0 +1,27 @@
+#include<iostream>//optimal approach of product of array with time complexicty condition
+#include<vector>
+#include<climits>
+using namespace std;
+vector<int>productExceptitself(vector<int>nums){
+    int n=nums.size();
+    vector<int>ans(n,1);
+    //prefix=>ans
+    for(int i=1;i<n;i++){
+        ans[i]=ans[i-1]*nums[i-1];
+    }
+    int suffix=1;
+    //suffix
+    for(int i=n-2;i>=0;i--){
+        suffix*=nums[i+1];
+        ans[i]*=suffix;
+    }
+    return ans;
+}
+int main(){
+    vector<int>nums={1,2,3,4};
+    vector<int>ans=productExceptitself(nums);
+    for(int x : ans){
+        cout<<x<<" ";// 24 12 8 6
+    }cout<<endl;
+    return 0;
+}
