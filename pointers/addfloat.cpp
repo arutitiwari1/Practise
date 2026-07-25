98e08f4bdaaed0a4d84f4e49fcf8023ff04f398a
Aruti <arutitiwari@gmail.com>
2026-07-25 20:27:03 +0530

    print address of float

---
+++ pointers/addfloat.cpp
@@ -0,0 +1,10 @@
+#include<iostream>//print address of float
+#include<vector>
+using namespace std;
+int  main(){
+    float price=100.25f;
+    float * ptr=&price;
+    cout<<&ptr<<endl;//0x61ff08
+    cout<<ptr<<endl;//ox61ff0c
+    return 0;
+}
