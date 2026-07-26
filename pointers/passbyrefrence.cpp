dc1b44231d5b825a873a5a95df8be03a97c40d4a
Aruti <arutitiwari@gmail.com>
2026-07-27 00:27:39 +0530

    pass by refrence

---
+++ pointers/passbyrefrence.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void changeA(int a){
+    a=20;
+}
+int main(){
+    int a=10;
+
+    changeA(a);
+    cout<<"inside main fnx:"<<a<<endl;//10
+    return 0;
+}
