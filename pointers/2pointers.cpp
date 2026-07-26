ec5f89d68e7e82da7db9ca39e9cd7002ea56db8f
Aruti <arutitiwari@gmail.com>
2026-07-26 23:46:13 +0530

    2pointers print

--- pointers/2pointers.cpp
+++ pointers/2pointers.cpp
@@ -2,5 +2,10 @@
 #include<vector>
 using namespace std;
 int main(){
-    
+    int a=10;
+    int *ptr=&a;
+    int **parPtr=&ptr;
+    cout<<&ptr<<endl;//0x61ff04
+    cout<<parPtr<<endl;//0x61ff04
+    return 0;
 }
