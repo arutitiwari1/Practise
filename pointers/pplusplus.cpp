0af96fd50872be16129b63d052518a00aeb06045
Aruti <arutitiwari@gmail.com>
2026-07-28 00:44:03 +0530

    p++ pointers

---
+++ pointers/pplusplus.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4,5};
+    int a=10;
+    int *ptr=&a;
+    cout<<ptr<<endl;//0x61fef4
+    ptr++;//+8B
+    cout<<ptr<<endl;//0x61fef8 change of 8
+    return 0;
+}
