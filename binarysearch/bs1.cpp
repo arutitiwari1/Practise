ad600016ad154261e3b24c08f7dbfca7bd2422cd
Aruti <arutitiwari@gmail.com>
2026-07-30 23:57:05 +0530

    prime no check

---
+++ binarysearch/bs1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int binarySearch(vector<int>arr,int tar){
+    int st=0,end=arr.size()-1;
+    while(st<=end){
+        //2nd half
+        int mid=st+(end-st)/2;
+        if ()   
+    }
+}
--- loops/prime.cpp
+++ loops/prime.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream>//prime no check
 using namespace std;
 int main(){
     int n;
