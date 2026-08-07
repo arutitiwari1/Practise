8aaf42f9e7262420120d9926b8c27bb6713cbf42
Aruti <arutitiwari@gmail.com>
2026-08-07 22:51:22 +0530

    square of two sides

---
+++ practicec/square.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+//area of square
+int main(){
+    int side;
+    printf("enter side");
+    scanf("%d",&side);//3
+    int area=side*side;
+    printf("area is %d",area);//9
+    return 0;
+}
