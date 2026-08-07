38e22272a1542efe551521d035104b4f08c0dbf1
Aruti <arutitiwari@gmail.com>
2026-08-07 21:59:10 +0530

    sum in c

---
+++ practicec/sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter a");
+    scanf("%d",&a);//3
+    printf("enter b");
+    scanf("%d",&b);//4
+    int sum=a+b;
+    printf("sum is %d",sum);//7
+    return 0;
+}
