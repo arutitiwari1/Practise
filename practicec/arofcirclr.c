85706cef4dd77e30230a6d8e3e65ffd4ff70f80a
Aruti <arutitiwari@gmail.com>
2026-08-08 19:48:24 +0530

    area of circle

---
+++ practicec/arofcirclr.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+//area of circle
+int main(){
+    int a,b;
+    printf("enter a:");//4
+    scanf("%d",&a);
+    printf("enter b:");//5
+    scanf("%d",&b);
+    printf("perimeter is %d",2*(a+b));//18
+    return 0;
+}
