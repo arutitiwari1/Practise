1f0de3f1594af886f2029fb3a7ed80c220d8a9f0
Aruti <arutitiwari@gmail.com>
2026-08-09 22:05:47 +0530

    areaofcircle

---
+++ practicec/areaofcircle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+//area of circle 
+float radius;
+printf("enter radius:");//3.2
+scanf("%f",&radius);
+printf("area is%f",3.14*radius*radius);//32.153601
+return 0;
+}
