b3174ef3f45aabf256c8157f3b7970bfa0862aa0
Aruti <arutitiwari@gmail.com>
2026-08-17 22:42:43 +0530

    merge sort array

---
+++ sort/mergesortarr.cpp
@@ -0,0 +1,28 @@
+#include<iostream>//leet code 88
+#include<vector>
+#include<climits>
+#include<algorithm>
+using namespace std;
+void merge(vector<int>&A,int m,vector<int>&B,int n){
+    int i=m-1,j=n-1,idx=m+n-1;
+    while(i>=0&&j>=0){
+        if (A[i]>=B[j]){
+            A[idx--]=A[i--];
+        }else{
+            A[idx--]=B[j--];
+        }
+    }while(j>=0){
+        A[idx--]=B[j--];
+    }
+}
+int main(){
+    vector<int>A={1,2,3,0,0,0};
+    int m=3;
+    vector<int>B={2,5,6};
+    int n=3;
+    merge(A,m,B,n);
+    for(int x:A){
+        cout<<x<<" ";// 1 2 2 3 5 6
+    }
+    return 0;
+}
