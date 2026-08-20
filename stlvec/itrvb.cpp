87bc839f78aea939c91c189d81cd7f9479821934
Aruti <arutitiwari@gmail.com>
2026-08-20 22:32:12 +0530

    iterior vec begin

---
+++ stlvec/itrvb.cpp
@@ -0,0 +1,8 @@
+#include<iostream>//iterior vec begin
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>vec={1,2,3,4,5};
+    cout<<"vec begin:"<<*(vec.begin())<<endl;//1
+    return 0;
+}
