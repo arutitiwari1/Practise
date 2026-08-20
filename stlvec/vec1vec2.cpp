bcff33768ccd1aaf8b514e85b5d400d9c44196e3
Aruti <arutitiwari@gmail.com>
2026-08-20 21:21:39 +0530

    vec1 vec 2 relatiion

---
+++ stlvec/vec1vec2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+ vector<int>vec1={1,2,3,4,5};
+ vector<int>vec2(vec1);
+ for(int val:vec2){
+    cout<<val<<" ";// 1 2 3 4 5
+ }
+ cout<<endl;
+ return 0;   
+}
