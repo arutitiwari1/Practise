91d12b3b3a87cdf9068f1ea0349c1fc925c2fd6c
Aruti <arutitiwari@gmail.com>
2026-08-22 15:07:11 +0530

    pair print

---
+++ pair/pairprnt.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+#include<deque>// no#include<pair> use
+using namespace std;
+int main(){
+pair<string,string>p={"who is"," aruti?"};
+cout << p.first;
+cout<< p.second <<endl;// who is aruti
+return 0;
+}
