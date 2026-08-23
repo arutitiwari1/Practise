17058b96b4ef2fe8c9312e12408a5aab5da99ff6
Aruti <arutitiwari@gmail.com>
2026-08-23 23:19:32 +0530

    pairsbusing empace nd push back

--- pair/prsusingitr.cpp
+++ pair/prsusingitr.cpp
@@ -7,7 +7,7 @@ vector<pair<int,int>>vec={{1,2},{2,3},{3,4}};
 vec.push_back({4,5}); // insert
 vec.emplace_back(4,5);//in place object creates
 for(auto p:vec){
-    cout<<p.first<<" "<<p.second<<endl;
+    cout<<p.first<<" "<<p.second<<endl;// 1 2 ; 2 3 ; 3 4 ;4 5 ; 4 5
 }
 cout<<endl;
 return 0;
